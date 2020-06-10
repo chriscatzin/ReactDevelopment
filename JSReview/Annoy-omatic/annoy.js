8bb1a547c5ccbd543273b878af8d693af677aec0
Chris Catzin <christophercatzin@Chriss-MacBook-Air.local>
2020-06-09 21:07:02 -0700

    files

---
+++ JSReview/Annoy-omatic/annoy.js
@@ -0,0 +1,10 @@
+var answer = prompt("Are we there yet?");
+while(answer !== "yes" && answer !== "yeah"){
+    var answer = "Are we there yet?";
+}
+
+if(answer === "yes"){
+    alert("YAY, we made it!");
+}else{
+
+}
