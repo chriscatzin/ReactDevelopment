5dbfc676dc0be7c4ef5f96e7199892dddbc48633
Chris Catzin <christophercatzin@Chriss-MacBook-Air.local>
2020-06-09 18:16:47 -0700

    files

--- JSReview/GuessingGame/guess.js
+++ JSReview/GuessingGame/guess.js
@@ -2,9 +2,12 @@ var secretNumber = 7;
 
 var guess = prompt("Guess a number");
 
-if(guess === secretNumber){
+if(Number(guess) === secretNumber){
     alert("YOU GOT IT RIGHT!");
 }
-else if(guess !== secretNumber){
-    alert("THE NUMBER IS WRONG!");
+else if(Number(guess) > secretNumber){
+    alert("The number is too high, guess again.");
+}
+else if(Number(guess) < secretNumber){
+    alert("The number is too low, guess again.")
 }
