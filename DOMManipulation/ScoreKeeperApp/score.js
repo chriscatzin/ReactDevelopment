6773a31d4aa73ba054fc64e471b7af19130397d9
Christopher Catzin <ckc196@humboldt.edu>
2020-06-21 16:09:33 -0700

    files

--- DOMManipulation/ScoreKeeperApp/score.js
+++ DOMManipulation/ScoreKeeperApp/score.js
@@ -1,5 +1,6 @@
 var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("p3");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display")
 var p1Score = 0;
@@ -12,6 +13,7 @@ p1Button.addEventListener("click", function(){
         p1Score++;
         if(p1Score === winningScore){
             gameOver = true;
+            p1Display.classList.add("winner");
         }
         p1Display.textContent = p1Score;
     }
@@ -22,9 +24,20 @@ p2Button.addEventListener("click", function(){
         p2Score++;
         if(p2Score === winningScore){
             gameOver = true;
+            p2Display.classList.add("winner");
         }
     }
     p2Display.textContent = p2Score;
 });
+
+resetButton.addEventListener("click", function(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+
+})
 
 
