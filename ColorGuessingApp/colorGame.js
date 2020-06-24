9839e7a022f7ec1680c3e5f28f6a9b7fa974cd26
Christopher Catzin <ckc196@humboldt.edu>
2020-06-23 20:45:38 -0700

    files

--- ColorGuessingApp/colorGame.js
+++ ColorGuessingApp/colorGame.js
@@ -6,3 +6,19 @@ var colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)",
 ]
+
+var squares = document.querySelector(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i=0; i < squares.length; i++){
+    // add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+
+    // add click listeners to squares
+    sqaures[i].addEventListener("click", function(){
+        
+    })
+}
