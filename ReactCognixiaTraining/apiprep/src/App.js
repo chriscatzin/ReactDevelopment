92845aad2005d8784e8ca31681bdeb8af163cef4
Christopher Catzin <ckc196@humboldt.edu>
2020-06-30 21:53:41 -0700

    files

---
+++ ReactCognixiaTraining/apiprep/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(getData => getData.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+  }
+
+  render(){
+
+    var{isLoaded, items} = this.state;
+    if(!isLoaded){
+      return <div>Loading...</div>;
+    }
+    else{
+
+      return (
+        <div>
+          
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                Name: {item.name} | Email: {item.email}
+              </li>
+            ))}
+          </ul>
+
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
