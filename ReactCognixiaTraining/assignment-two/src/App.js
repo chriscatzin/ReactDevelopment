4ec342f69093706fd8b8c626a1dcf60d21c481eb
Christopher Catzin <ckc196@humboldt.edu>
2020-07-21 23:04:21 -0700

    files

--- ReactCognixiaTraining/assignment-two/src/App.js
+++ ReactCognixiaTraining/assignment-two/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -46,11 +47,16 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -68,12 +74,26 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'lightred',
+        color: 'black'
+      }
+    }
+
+    let classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red'); //classes = ['red']
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold'); // classes = ['red','bold']
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
@@ -84,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
