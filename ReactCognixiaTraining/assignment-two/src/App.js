ed6f99cea8c1eb203b62a1c74eefbee13a2b8c46
Christopher Catzin <ckc196@humboldt.edu>
2020-07-28 22:46:00 -0700

    files

--- ReactCognixiaTraining/assignment-two/src/App.js
+++ ReactCognixiaTraining/assignment-two/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';\
-// Chris Catzin 7/27/20
+// Chris Catzin 7/28/20
 
 class App extends Component {
   state = {
